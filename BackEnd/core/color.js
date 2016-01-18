0ab87a70be8f120256d030ac762083deed72f7d2
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-01-18 09:56:23 +0100

    Class user a peu prés rdy et ajout de generator color et id

---
+++ BackEnd/core/color.js
@@ -0,0 +1,15 @@
+/**
+ * Created by guillaume on 18/01/2016.
+ */
+
+/**
+ * This function is a color maker.
+ * It makes unique color id.
+ */
+function* colorMaker (){
+    var colorId = 0;
+    while(colorId >= 0)
+        yield colorId++;
+}
+
+module.exports = colorMaker();
--- BackEnd/core/user.js
+++ BackEnd/core/user.js
@@ -4,10 +4,26 @@
  * Created by guillaume on 18/01/2016.
  */
 
+var idGenerator = require('./id.js')();
+var colorIdGenerator = require('./color.js')();
+
 /**
  * This class represent a user. The one that has the watch and the smartphone.
  */
 class User {
+
+    constructor () {
+        this._id = idGenerator.next().value;
+        this._color = colorIdGenerator.next().value;
+    }
+
+    get id () {
+        return this._id;
+    }
+
+    get color () {
+        return this._color;
+    }
 
 }
 
