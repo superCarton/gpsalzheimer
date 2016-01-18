200f43bd0667f5885a82ba79d25d4ece67828f10
Romain Guillot <rguillot1003@gmail.com>
2016-01-18 17:03:29 +0100

    Android socket io

---
+++ GPSAlzheimerPatient/mobile/src/main/java/polytech/gpsalzheimer/Events.java
@@ -0,0 +1,10 @@
+package polytech.gpsalzheimer;
+
+/**
+ * Created by Romain Guillot on 18/01/16.
+ */
+public class Events {
+    public static final String WAITING_TO_CONNECT_TAB = "polytech.gpsalzheimer.waitingtab";
+    public static final String ADDED_ON_TAB = "polytech.gpsalzheimer.addedontab";
+
+}
