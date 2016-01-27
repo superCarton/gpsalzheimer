b21644fd71d421618507dd8c870c7c64c0a42e24
NablaT <remi.pourtier@laposte.net>
2016-01-27 14:53:13 +0100

    refactoring socket.js and mapCtrl, rewriting methods for socket.io

--- tablet/www/js/controllers/mapCtrl.js
+++ tablet/www/js/controllers/mapCtrl.js
@@ -1,8 +1,7 @@
 'use strict';
 
-angular.module('starter').controller('MapCtrl',  function ($scope, $state, $cordovaGeolocation, $ionicLoading, socket) {
+angular.module('starter').controller('MapCtrl', function ($scope, $state, $cordovaGeolocation, $ionicLoading, constants) {
   var options = {timeout: 10000, enableHighAccuracy: true};
-
   // Setup the loader
   $ionicLoading.show({
     content: 'Loading',
@@ -27,36 +26,53 @@ angular.module('starter').controller('MapCtrl',  function ($scope, $state, $cord
     var image = 'img/localisation.png';
     var image2 = 'img/localisation2.png';
     var image3 = 'img/localisation3.png';
-    var imageList=[image,image2,image3];
+    var imageList = [image, image2, image3];
 
     $scope.personList;
     $scope.markerList;
 
+    var socket;
+    socket = io.connect(constants.backendUrl, function () {
+      console.log("Connection success");
+    });
 
+    socket.io.on('connect_error', function (err) {
+      console.log('Error connecting to server');
+    });
+
+    socket.on("updateUsers", function (params) {
+      if (params !== {}) {
+        console.log('listenPerson: ', params);
+        $scope.personList = params;
+        console.log('scope person list:', $scope.personList);
+        initMarkers(params);
+      }
+    });
     /**
      * We start the connection
-     */
-    socket.connect();
+     *
+     socket=connect(socket);
 
-    /**
+     listenPerson(socket);
+     /**
      * We get back the person list from the server.
-     */
-    socket.getPerson().then(
-      function(data){
+
+     socket.getPerson().then(
+     function(data){
         console.log("data person list: ",data);
         $scope.personList=data;
         initMarkers();
       },
-      function (msg){
+     function (msg){
         console.log("error getbackscore homepage",msg);
       }
-    );
+     );*/
 
     /**
      * Function initMarkers. This function initializes the marker on the map (used to see user localisation) according to the
      * list of people we have.
-     */
-    $scope.initMarkers= function(){
+     *
+     $scope.initMarkers= function(){
       var allMarker= new Array();
       $scope.cleanMarkers(null);
       for(var i=0; i<$scope.personList.length;i++){
@@ -71,48 +87,71 @@ angular.module('starter').controller('MapCtrl',  function ($scope, $state, $cord
     }
 
 
-    /**
+     /**
      * Function cleanMarkers. This function is used to clean our marker (make them disapear on the map). To ensure this action,
      * the parameter map has to be null.
      * @param map
      */
-    $scope.cleanMarkers=function(map){
-      for(var i=0; i<$scope.markerList.length;i++){
+    $scope.cleanMarkers = function (map) {
+      for (var i = 0; i < $scope.markerList.length; i++) {
         $scope.markerList[i].setMap(map);
       }
     }
 
+
+    /* $scope.connect=function(){
+     //socket = io.connect(constants.backendUrl, {"path":"/gpsalzheimer/socket.io"}, function(){
+     socket = io.connect(constants.backendUrl, function(){
+     console.log("Connection success");
+     });
+
+     socket.io.on('connect_error', function (err) {
+     console.log('Error connecting to server');
+     });
+     }
+
+     $scope.listenPerson=function(){
+     socket.on("updateGpsData", function(params){
+     if(params !=={}){
+     console.log('listenPerson',params);
+     $scope.personList=params;
+     }
+     });
+     }
+
+     */
+
     /*
 
-    var marker = new google.maps.Marker({
-      position: {lat: 43.615377, lng: 7.07185},
-      animation: google.maps.Animation.DROP,
-      map: map,
-      icon: image
-    });
-    //marker.addListener('click', toggleBounce);
+     var marker = new google.maps.Marker({
+     position: {lat: 43.615377, lng: 7.07185},
+     animation: google.maps.Animation.DROP,
+     map: map,
+     icon: image
+     });
+     //marker.addListener('click', toggleBounce);
 
 
-    var marker2 = new google.maps.Marker({
-      position: {lat: 43.6154, lng: 7.07189},
-      animation: google.maps.Animation.DROP,
-      map: map,
-      icon: image
-    });
-   // marker2.addListener('click', toggleBounce);
+     var marker2 = new google.maps.Marker({
+     position: {lat: 43.6154, lng: 7.07189},
+     animation: google.maps.Animation.DROP,
+     map: map,
+     icon: image
+     });
+     // marker2.addListener('click', toggleBounce);
 
-    var marker3 = new google.maps.Marker({
-      position: {lat: 43.6155, lng: 7.0719},
-      animation: google.maps.Animation.DROP,
-      map: map,
-      icon: image
-    });
-   // marker3.addListener('click', toggleBounce);
+     var marker3 = new google.maps.Marker({
+     position: {lat: 43.6155, lng: 7.0719},
+     animation: google.maps.Animation.DROP,
+     map: map,
+     icon: image
+     });
+     // marker3.addListener('click', toggleBounce);
 
-*/
+     */
 
     //Wait until the map is loaded
-    google.maps.event.addListenerOnce(map, 'idle', function(){
+    google.maps.event.addListenerOnce(map, 'idle', function () {
 
 
       var mar = new google.maps.Marker({
@@ -144,6 +183,48 @@ angular.module('starter').controller('MapCtrl',  function ($scope, $state, $cord
       for (var i = 0; i < neighborhoods.length; i++) {
         addMarkerWithTimeout(neighborhoods[i], i * 200);
       }
+    }
+
+    function initMarkers(userList) {
+      console.log('in initmarker');
+      console.log("user list", userList);
+      var allMarker = new Array();
+      //$scope.cleanMarkers(null);
+      console.log("user list 0", userList.users[0]);
+
+      for (var i = 0; i < userList.users.length; i++) {
+        console.log("--------------------- I ",i,"------------------------");
+        console.log("latitude", userList.users[i]._position._latitude);
+        console.log("longitude", userList.users[i]._position._longitude);
+        console.log("----------------------------------------------------");
+        var marker = new google.maps.Marker({
+          position: {lat: userList.users[i]._position._latitude, lng: userList.users[i]._position._longitude},
+          map: map,
+          icon: imageList[i]
+        });
+        allMarker.push(marker);
+      }
+      $scope.markerList = allMarker;
+    }
+
+    function connect(socket) {
+      //socket = io.connect(constants.backendUrl, {"path":"/gpsalzheimer/socket.io"}, function(){
+      socket = io.connect(constants.backendUrl, function () {
+        console.log("Connection success");
+      });
+
+      socket.io.on('connect_error', function (err) {
+        console.log('Error connecting to server');
+      });
+    }
+
+    function listenPerson(socket) {
+      socket.on("updateGpsData", function (params) {
+        if (params !== {}) {
+          console.log('listenPerson', params);
+          $scope.personList = params;
+        }
+      });
     }
 
 
